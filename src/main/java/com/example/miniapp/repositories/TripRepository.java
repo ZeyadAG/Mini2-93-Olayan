1f099757fb5cd05c699bb9c431deae4964138126
seifzakaria2 <seifzakaria2@gmail.com>
2025-04-25 01:08:17 +0300

    added some classes

--- src/main/java/com/example/miniapp/repositories/TripRepository.java
+++ src/main/java/com/example/miniapp/repositories/TripRepository.java
@@ -1,5 +1,16 @@
 package com.example.miniapp.repositories;
 
-public class TripRepository {
+import com.example.miniapp.models.Trip;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface TripRepository extends JpaRepository<Trip, Long> {
+
+    // Find trips between two tripDate values
+    List<Trip> findByTripDateBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+    // Find all trips for a specific captain
+    List<Trip> findByCaptainId(Long captainId);
 }
